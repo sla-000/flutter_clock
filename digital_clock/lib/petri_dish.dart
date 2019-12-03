a1bd10f2415579491b12847d2f0e66528732e9a4
sla000 <ryabinin.v.v@gmail.com>
2019-12-03 23:22:49 +0300

    Add flame, make rotating cell

--- digital_clock/lib/petri_dish.dart
+++ digital_clock/lib/petri_dish.dart
@@ -1,7 +1,11 @@
+import 'dart:math' as math;
 import 'dart:ui' as ui;
 
+import 'package:flame/anchor.dart';
+import 'package:flame/components/component.dart';
+import 'package:flame/game.dart';
+import 'package:flame/palette.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:logging/logging.dart';
 
 import 'assets.dart' as assets;
@@ -21,7 +25,7 @@ class PetriDish extends StatefulWidget {
 }
 
 class _PetriDishState extends State<PetriDish> {
-  int _frameCallbackId;
+  MyGame myGame = MyGame();
 
   @override
   void initState() {
@@ -38,47 +42,33 @@ class _PetriDishState extends State<PetriDish> {
 
   @override
   Widget build(BuildContext context) {
-    return FittedBox(
-      child: ValueListenableBuilder<bool>(
-        valueListenable: assets.loaded,
-        builder: (BuildContext context, bool loaded, Widget child) {
-          if (!loaded) {
-            return child;
-          }
+    return ValueListenableBuilder<bool>(
+      valueListenable: assets.loaded,
+      builder: (BuildContext context, bool loaded, Widget child) {
+        if (!loaded) {
+          return child;
+        }
 
-          return CustomPaint(
-            painter: PetriPainter(),
-            size: const Size(500, 1000),
-          );
-        },
-        child: Container(
-          color: Colors.black,
-          width: 50,
-          height: 100,
-        ),
+        return AspectRatio(
+          aspectRatio: 1 / 2,
+          child: Padding(
+            padding: const EdgeInsets.all(2.0),
+            child: myGame.widget,
+          ),
+        );
+
+        return CustomPaint(
+          painter: PetriPainter(),
+          size: const Size(500, 1000),
+        );
+      },
+      child: Container(
+        color: Colors.black,
+        width: 50,
+        height: 100,
       ),
     );
   }
-
-  void _scheduleTick() {
-    _frameCallbackId = SchedulerBinding.instance.scheduleFrameCallback(_tick);
-  }
-
-  void _unscheduleTick() {
-    SchedulerBinding.instance.cancelFrameCallbackWithId(_frameCallbackId);
-  }
-
-  void _tick(Duration timestamp) {
-    if (!mounted) {
-      return;
-    }
-    _scheduleTick();
-    _update(timestamp);
-
-    setState(() {});
-  }
-
-  void _update(Duration timestamp) {}
 }
 
 class PetriPainter extends CustomPainter {
@@ -88,93 +78,130 @@ class PetriPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
 //    _log.finest(() => 'paint: size=$size');
 
-    int delta = 0;
-
-    final int currentMillis = DateTime
-        .now()
-        .toUtc()
-        .millisecondsSinceEpoch;
-    if (lastMillis != null) {
-      delta = currentMillis - lastMillis;
-    }
-    lastMillis = currentMillis;
-
     canvas.drawRect(
         const Rect.fromLTWH(3.0, 3.0, 500.0 - 6.0, 1000.0 - 6.0), Paint());
-
-    _drawActor(
-      canvas,
-      assets.bodyImage,
-      Offset(size.width / 2, size.height / 2),
-      Paint()..color = Colors.white,
-      scale: const Size(3.0, 3.0),
-      rotation: delta / 1000 * 0.1,
-    );
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
     return true;
   }
+}
 
-  void _drawActor(Canvas canvas,
-      ui.Image image,
-      Offset offset,
-      Paint paint, {
-        Size scale = const Size(1.0, 1.0),
-        double rotation = 0,
-      }) {
-    canvas.save();
+class Palette {
+  static const PaletteEntry white = BasicPalette.white;
+  static const PaletteEntry red = PaletteEntry(Color(0xFFFF0000));
+  static const PaletteEntry blue = PaletteEntry(Color(0xFF0000FF));
+}
 
-    canvas.translate(offset.dx, offset.dy);
+class Cell extends PositionComponent {
+  static const SPEED = 0.25;
 
-    canvas.rotate(rotation);
-
-    canvas.scale(scale.width, scale.height);
-
-    canvas.drawImage(image, Offset(-image.width / 2, -image.height / 2), paint);
-
-    _drawTail(canvas);
-
-    _drawEye(canvas);
-
-    canvas.restore();
+  Cell(double size) {
+    width = height = size;
+    anchor = Anchor.center;
   }
 
-  void _drawTail(Canvas canvas) {
-    canvas.save();
+  @override
+  void resize(Size size) {
+    x = size.width / 2;
+    y = size.height / 2;
 
-    canvas.translate(40, 40);
+    width = height = math.min(size.width, size.height) / 2;
+  }
 
-    canvas.rotate(0);
+  @override
+  void render(Canvas c) {
+    prepareCanvas(c);
 
-    canvas.scale(1, 1);
-
-    canvas.drawImage(
-      assets.tailImage,
-      Offset(0, 0),
+    _drawActor(
+      c,
+      assets.bodyImage,
+      Offset(width / 2, height / 2),
       Paint()
-        ..colorFilter = ColorFilter.mode(Colors.green[600], BlendMode.modulate),
+        ..color = Colors.white,
+      scale: const Size(0.5, 0.5),
+      rotation: 0.1,
     );
-
-    canvas.restore();
   }
 
-  void _drawEye(Canvas canvas) {
-    canvas.save();
-
-    canvas.translate(-18, -18);
-
-    canvas.rotate(3.14 / 4);
-
-    canvas.scale(1, 1);
-
-    canvas.drawImage(
-      assets.eyeImage,
-      Offset(-assets.eyeImage.width / 2, -assets.eyeImage.height / 2),
-      Paint(),
-    );
-
-    canvas.restore();
+  @override
+  void update(double t) {
+    angle += SPEED * t;
+    angle %= 2 * math.pi;
   }
+}
+
+class MyGame extends BaseGame {
+  MyGame() {
+    add(Cell(64.0));
+  }
+
+  @override
+  void resize(ui.Size size) {
+    super.resize(size);
+
+    _log.finest(() => 'resize: size=$size');
+  }
+}
+
+void _drawActor(Canvas canvas,
+    ui.Image image,
+    Offset offset,
+    Paint paint, {
+      Size scale = const Size(1.0, 1.0),
+      double rotation = 0,
+    }) {
+  canvas.save();
+
+  canvas.translate(offset.dx, offset.dy);
+
+  canvas.rotate(rotation);
+
+  canvas.scale(scale.width, scale.height);
+
+  canvas.drawImage(image, Offset(-image.width / 2, -image.height / 2), paint);
+
+  _drawTail(canvas);
+
+  _drawEye(canvas);
+
+  canvas.restore();
+}
+
+void _drawTail(Canvas canvas) {
+  canvas.save();
+
+  canvas.translate(40, 40);
+
+  canvas.rotate(0);
+
+  canvas.scale(1, 1);
+
+  canvas.drawImage(
+    assets.tailImage,
+    Offset(0, 0),
+    Paint()
+      ..colorFilter = ColorFilter.mode(Colors.green[600], BlendMode.modulate),
+  );
+
+  canvas.restore();
+}
+
+void _drawEye(Canvas canvas) {
+  canvas.save();
+
+  canvas.translate(-18, -18);
+
+  canvas.rotate(3.14 / 4);
+
+  canvas.scale(1, 1);
+
+  canvas.drawImage(
+    assets.eyeImage,
+    Offset(-assets.eyeImage.width / 2, -assets.eyeImage.height / 2),
+    Paint(),
+  );
+
+  canvas.restore();
 }
