310372f8df2194232fb4977b1c38b102327510a0
Vyacheslav Ryabinin <vyacheslav.ryabinin@hiq.ru>
2020-01-03 23:07:47 +0300

    Stubs for draw line and circle functions

--- digital_clock/lib/actors/scene/digits.dart
+++ digital_clock/lib/actors/scene/digits.dart
@@ -1,6 +1,10 @@
 import 'package:digital_clock/engine/actor.dart';
 import 'package:digital_clock/engine/vector.dart';
 
-void line(List<Actor> children, Vector vector0, Vector vector1) {}
+void line(List<Actor> children, Vector vector0, Vector vector1) {
+  // todo line
+}
 
-void circle(List<Actor> children, Vector center, double radius) {}
+void circle(List<Actor> children, Vector center, double radius) {
+  // todo circle
+}
